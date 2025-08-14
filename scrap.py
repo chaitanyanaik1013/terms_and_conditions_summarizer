69caf5bbe06b9b961394c38b01e2131b6811c621
Chaitanya Naik <naikchaitanyaabc@gmail.com>
2025-08-14 12:11:36 +0000

    Working python script

--- scrap.py
+++ scrap.py
@@ -1,13 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
+from google import genai
+from google.genai import types
 
 link = input()
 
 url = f"{link}"
-response = requests.get(url)
+page_response = requests.get(url)
 
-soup = BeautifulSoup(response.text, "html.parser")
+soup = BeautifulSoup(page_response.text, "html.parser")
 page_text = soup.get_text()
 
-with open("tnc.txt", "w") as file:
-    file.write(page_text)
+#Sending the file to the google gemini api
+
+client = genai.Client(api_key="AIzaSyBG0i4nXf09Yu-e9eEs2mMX1ilwf5gdBsM")
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    config=types.GenerateContentConfig(
+        system_instruction="Act as a risk analyst for a small " \
+        "business owner. Analyze the following Terms and " \
+        "Conditions text. First, provide a summary in 10 " \
+        "brief and concise bullet points. Second, and most importantly, " \
+        "identify the potential risks from clauses related to " \
+        "these four specific risk categories: 1. Data Privacy "
+        "and Usage Rights. 2. User Content Ownership and IP. " \
+        "3. Account Suspension and Termination Conditions. " \
+        "4. Subscription Renewals and Price Changes in a very brief bullet points. If a " \
+        "category is not mentioned, state 'No specific clauses " \
+        "found.' Present the output clearly, with the summary " \
+        "first, followed by the four risk categories."),
+    contents=page_text
+)
+print(response.text)
